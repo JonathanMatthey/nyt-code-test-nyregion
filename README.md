993e065eb8b5bac956f3997472b89c9c0d466d40
Jonathan Matthey <mattheyj@gmail.com>
2013-07-08 13:57:46 -0400

    added readme and welcome page

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 nyt-code-test-nyregion
 ======================
 
-hi
+This is Rails 4 project, on ruby 2.0.0-p0
 
+I can host it on heroku if you'd like.
+
+to run it locally, run:
+
+	rails s
+
+useful links:
+
+rbenv: https://github.com/sstephenson/rbenv
+
+rails: http://rubyonrails.org/
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 NytCodeTestNyregion::Application.routes.draw do
+  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  root "welcome#index"
 
   post '/news', to: 'news#render_403'
   put '/news', to: 'news#render_403'
